7a6be6a7bc5a74c676cb996f1ff46e012286be48
nabeellagi <nabeellagi@github.com>
2025-05-16 15:21:08 +0700

    Adding WiFi Portal

--- IoT/sketch/sketch.ino
+++ IoT/sketch/sketch.ino
@@ -4,34 +4,98 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include "DHT.h"
+#include <LittleFS.h>
+#include <ESPAsyncWebServer.h>
 
-const char* ssid = "WIFI";
-const char* password = "PASSWORD";
+// Server
+AsyncWebServer server(80);
 
-const char* serverLocation = "http://192.168.60.19:8000/location?device_id=test";
-const char* serverSensor = "http://192.168.60.19:8000/sensor?device_id=test";
-const char* serverFlame = "http://192.168.60.19:8000/fire-detection?device_id=test";
-const char* serverTemperatureSet = "http://192.168.60.19:8000/temperature-set?device_id=test";
-const char* serverLamp = "http://192.168.60.19:8000/sensor/lamp?device_id=test";
+// WiFi Config
+String ssid = "";
+String pass = "";
 
-#define FLAME_SENSOR_PIN 4  
-#define LED_PIN 2           
+// Config file paths
+String ssidPath = "/ssid.txt";
+String passPath = "/pass.txt";
+
+// Endpoints
+const char* serverLocation = "http://192.168.42.196:8000/location?device_id=test";
+const char* serverSensor = "http://192.168.42.196:8000/sensor?device_id=test";
+const char* serverFlame = "http://192.168.42.196:8000/fire-detection?device_id=test";
+const char* serverTemperatureSet = "http://192.168.42.196:8000/temperature-set?device_id=test";
+const char* serverLamp = "http://192.168.42.196:8000/sensor/lamp?device_id=test";
+
+// Pins
+#define FLAME_SENSOR_PIN 4
+#define LED_PIN 2
 #define DHTPIN 21
 #define DHTTYPE DHT22
 #define DIMMER_PIN 23
 
+// GPS
 static const int RXPin = 18, TXPin = 19;
 static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(1);
 
+// DHT
 DHT dht(DHTPIN, DHTTYPE);
 
-float temperatureSetpoint = 29.50;  // default nilai awal
+// Temperature setpoint
+float temperatureSetpoint = 29.50;
+
+// Form input names
+const char* PARAM_INPUT_1 = "ssid";
+const char* PARAM_INPUT_2 = "pass";
+
+// File read/write functions
+String readFile(fs::FS &fs, const String &path) {
+  File file = fs.open(path.c_str(), "r");
+  if (!file || file.isDirectory()) return "";
+  String content = file.readString();
+  file.close();
+  return content;
+}
+
+void writeFile(fs::FS &fs, const String &path, const String &content) {
+  File file = fs.open(path.c_str(), "w");
+  if (!file) return;
+  file.print(content);
+  file.close();
+}
+
+// WiFi connection logic
+bool initWiFi() {
+  if (ssid == "") {
+    Serial.println("WiFi SSID kosong.");
+    return false;
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), pass.c_str());
+
+  Serial.print("Mencoba konek ke WiFi: ");
+  Serial.println(ssid);
+
+  unsigned long startAttemptTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nBerhasil konek WiFi!");
+    Serial.print("Alamat IP: ");
+    Serial.println(WiFi.localIP());
+    return true;
+  } else {
+    Serial.println("\nGagal konek WiFi.");
+    return false;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-
   pinMode(FLAME_SENSOR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(DIMMER_PIN, OUTPUT);
@@ -40,15 +104,97 @@ void setup() {
 
   dht.begin();
 
-  WiFi.begin(ssid, password);
-  Serial.print("Menghubungkan ke WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if (!LittleFS.begin()) {
+    Serial.println("Gagal mount LittleFS");
+    return;
   }
-  Serial.println("\nWiFi terhubung");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+
+  ssid = readFile(LittleFS, ssidPath);
+  Serial.println("SSID: " + ssid);
+  pass = readFile(LittleFS, passPath);
+  Serial.println("Password: " + pass);
+
+  if (!initWiFi()) {
+    Serial.println("Gagal konek WiFi. Membuat Access Point...");
+    WiFi.softAP("ThermoFarm Setel WiFi");
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("Alamat AP: ");
+    Serial.println(IP);
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+      request->send(LittleFS, "/index.html", "text/html");
+    });
+
+    server.on("/success", HTTP_GET, [](AsyncWebServerRequest *request) {
+      request->send(LittleFS, "/success.html", "text/html");
+    });
+
+    server.on("/bulma.min.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+      request->send(LittleFS, "/bulma.min.css", "text/css");
+    });
+
+    server.on("/nerd.png", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(LittleFS, "/nerd.png", "image/png");
+    });
+
+
+    // Mount POST handler regardless of WiFi mode
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+      int params = request->params();
+      for (int i = 0; i < params; i++) {
+        const AsyncWebParameter* p = request->getParam(i);
+        if (p->isPost()) {
+          if (p->name() == PARAM_INPUT_1) {
+            ssid = p->value().c_str();
+            writeFile(LittleFS, ssidPath, ssid);
+          }
+          if (p->name() == PARAM_INPUT_2) {
+            pass = p->value().c_str();
+            writeFile(LittleFS, passPath, pass);
+          }
+        }
+      }
+
+      // 🔁 Try connecting with new credentials
+      WiFi.begin(ssid.c_str(), pass.c_str());
+
+      unsigned long startAttemptTime = millis();
+      bool wifiConnected = false;
+      while (millis() - startAttemptTime < 5000) {  // wait up to 5 seconds
+        if (WiFi.status() == WL_CONNECTED) {
+          wifiConnected = true;
+          break;
+        }
+        delay(500);
+      }
+
+      if (wifiConnected) {
+        Serial.println("WiFi berhasil disambungkan dari POST!");
+        request->send(LittleFS, "/success.html", "text/html");
+
+        // Restart ESP to apply WiFi
+        xTaskCreate(
+          [](void* param) {
+            delay(3000);
+            ESP.restart();
+          },
+          "RestartTask",
+          2048,
+          NULL,
+          1,
+          NULL
+        );
+      } else {
+        Serial.println("Gagal konek dengan kredensial baru!");
+        request->send(LittleFS, "/error.html", "text/html");
+      }
+    });
+
+
+  } else {
+    Serial.println("WiFi Terkoneksi: " + WiFi.localIP().toString());
+  }
+  server.begin();
 }
 
 void loop() {
